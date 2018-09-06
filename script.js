c244cdf0d70f55e9c613f1911f6a4969541b39b3
yousifsaleh <yousifsaleh3@icloud.com>
2018-09-06 13:35:28 -0400

    Lab

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-function fillMessage()
-{
+function fillMessage() {
 
  	let words = [];
 
-	var N = document.getElementById("number").value;
+	var n = document.getElementById("number").value;
 
-var output="Triangles: ";
+var output = "Triangles: </br>";
 
   for (let c = 1; c <= n; c++) {
-  for (let b = 1; b < c; b++) {
-  for (let a = 1; a < b; a++) {
-  if (a * a + b * b === c * c) {
-    output = output + (`${a}, ${b}, ${c}` + "</br>");
-  }
-  }
-  }
-  }
+    for (let b = 1; b < c; b++) {
+        for (let a = 1; a < b; a++) {
+            if (a * a + b * b === c * c) {
+                output = output + (`${a}, ${b}, ${c}` + "</br>");
+              }
+            }
+          }
+}
 
-	document.getElementById('output').innerHTML = msg[0];
+	document.getElementById('output').innerHTML = output;
 
 }
